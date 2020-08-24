9462adb9ff2f9d2a86feeccd51e9d4252f60826d
zhiqiang_ye <zhiqiang_ye@foxitsoftware.cn>
2020-08-24 18:20:36 +0800

    fix mas_safeAreaLayoutGuide crash & update version 1.1.1

--- Masonry/View+MASAdditions.m
+++ Masonry/View+MASAdditions.m
@@ -137,7 +137,7 @@
 #if (__IPHONE_OS_VERSION_MAX_ALLOWED >= 110000) || (__TV_OS_VERSION_MAX_ALLOWED >= 110000)
 
 - (MASViewAttribute *)mas_safeAreaLayoutGuide {
-    return [[MASViewAttribute alloc] initWithView:self item:self.safeAreaLayoutGuide layoutAttribute:NSLayoutAttributeBottom];
+    return [[MASViewAttribute alloc] initWithView:self item:self.safeAreaLayoutGuide layoutAttribute:NSLayoutAttributeNotAnAttribute];
 }
 - (MASViewAttribute *)mas_safeAreaLayoutGuideTop {
     return [[MASViewAttribute alloc] initWithView:self item:self.safeAreaLayoutGuide layoutAttribute:NSLayoutAttributeTop];
